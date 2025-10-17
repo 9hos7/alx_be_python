d0f11897cf48756932be3f5cf1b1247685a7f8f8
Dilinna <anoziedilinna12@gmail.com>
2025-10-17 22:48:29 +0100

    initial commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,12 +4,16 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
+        return self.account_balance
 
     def withdraw(self, amount):
-        if self.account_balance > amount:
+        if amount <= self.account_balance:
             self.account_balance -= amount
+            return True
         else:
+            self.account_balance -= 0
             print("insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f'Current Balance: ${self.account_balance}')
