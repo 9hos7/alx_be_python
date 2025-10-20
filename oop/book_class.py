23d890b47acdaa77902521c3b688f7b3d94fe19e
Dilinna <anoziedilinna12@gmail.com>
2025-10-20 12:03:45 +0100

    another one

---
+++ oop/book_class.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, author, title, year):
+        self.author = author
+        self.title = title
+        self.publication_year = int(year)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.publication_year}"
+    
+    def __repr__(self):
+        return f"Book ('{self.title}', '{self.author}', {self.publication_year})"
+    
+    def __del__(self):
+        print(f"Deleting {self.title}")
